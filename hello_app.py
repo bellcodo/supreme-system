dc6638cc7c2ad485f9c7d92929b25dedaefd67ec
Michael Schober <mschober@expedia.com>
2016-03-13 04:31:43 -0700

    adding post

--- hello_app.py
+++ hello_app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/post/username/<string:uname>/activity/<string:activity>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
